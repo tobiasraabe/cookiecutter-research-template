986d963337d703bf9455b1249dc469116ab3a997
Tobias Raabe <tobiasraabe@uni-bonn.de>
2018-08-09 19:44:34 +0200

    fix.

--- {{cookiecutter.project_slug}}/src/documentation/conf.py
+++ {{cookiecutter.project_slug}}/src/documentation/conf.py
@@ -35,7 +35,7 @@ extensions = [
     'nbsphinx',
 ]
 
-autodoc_mock_imports = ['bld', 'numpy']
+autodoc_mock_imports = ['bld', 'matplotlib', 'numpy']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
