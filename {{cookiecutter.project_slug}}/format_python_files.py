8b6bb8672e3741eaf1b9dfd85db2e415f4a3c181
Tobias Raabe <tobiasraabe@users.noreply.github.com>
2018-08-11 20:37:19 +0200

    Improved testing (#7)

--- {{cookiecutter.project_slug}}/format_python_files.py
+++ {{cookiecutter.project_slug}}/format_python_files.py
@@ -12,11 +12,11 @@ def cli():
 
     """
     click.echo('Start reformatting files with isort.')
-    os.system('isort . -rc -sp tox.ini')
+    os.system('isort . -rc')
     click.echo('Start reformatting files with black.')
     os.system('black .')
     click.echo('Start reformatting files with isort again for commas.')
-    os.system('isort . -rc -sp tox.ini')
+    os.system('isort . -rc')
     click.echo('End reformatting files.')
 
 
