d6074b8256458778f97f7ae5e484939da9b67403
Tobias Raabe <tobiasraabe@users.noreply.github.com>
2018-10-16 20:58:59 +0200

    Enhance conda environment handling. Fixed #11, fixed #14. (#21)

--- tests/test_cookie.py
+++ tests/test_cookie.py
@@ -1,5 +1,7 @@
 import sys
 
+import pytest
+
 
 def test_bake_project(cookies):
     major, minor = sys.version_info[:2]
@@ -19,16 +21,17 @@ def test_bake_project(cookies):
 
 
 def test_remove_downloader(cookies):
-    result = cookies.bake(extra_context={"add_downloader": "n"})
+    result = cookies.bake(extra_context={"add_downloader": "no"})
 
     downloader = result.project.join("prepare_data_for_project.py")
 
     assert result.exit_code == 0
+    assert result.exception is None
     assert downloader.check(exists=0)
 
 
 def test_remove_formatter(cookies):
-    result = cookies.bake(extra_context={"add_formatter": "n"})
+    result = cookies.bake(extra_context={"add_formatter": "no"})
 
     formatter = result.project.join("format_python_files.py")
     pyproject = result.project.join("pyproject.toml")
@@ -38,3 +41,30 @@ def test_remove_formatter(cookies):
     assert formatter.check(exists=0)
     assert pyproject.check(exists=0)
     assert "[isort]" not in tox
+
+
+@pytest.mark.skipif(
+    sys.version_info[:2] != (3, 7),
+    reason="Miniconda is only installed for Python 3.7",
+)
+def test_check_conda_environment_creation(cookies):
+    result = cookies.bake(
+        extra_context={
+            "create_conda_environment_at_finish": "yes",
+            "conda_environment_name": "test",
+        }
+    )
+
+    assert result.exit_code == 0
+    assert result.exception is None
+
+    # TODO: Test that the environment can be activated. Currently, the
+    # following test fails as the extracted prefix is "pytest". Maybe the
+    # activation is not working.
+
+    # if platform.system() == "Windows":
+    #   os.system("activate test")
+    # else:
+    #   os.system("source activate test")
+
+    # assert sys.prefix.split(os.path.sep)[-1] == "test"
