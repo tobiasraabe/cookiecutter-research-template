a6e00ced43dc05989c2e02eda2c6d35eb872edfa
Tobias Raabe <tobiasraabe@uni-bonn.de>
2018-10-16 19:23:41 +0200

    fix.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":  # noqa: C901
 
     if "{{ cookiecutter.create_conda_environment_at_finish }}" == "yes":
         os.system(
-            "conda create "
+            "conda-env create "
             "-f {{ cookiecutter.project_slug }}/environment.yml "
             "-n {{ cookiecutter.conda_environment_name }}"
         )
